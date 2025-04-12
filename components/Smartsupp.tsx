fe0f8874cf6c6eb40d665020d7399b1ee50e03c6
samie105 <samsonrichfield@gmail.com>
2025-04-12 16:54:49 +0100

    fixxx

--- components/Smartsupp.tsx
+++ components/Smartsupp.tsx
@@ -1,35 +1,35 @@
-// "use client";
+"use client";
 
-// import { useEffect } from "react";
+import { useEffect } from "react";
 
-// declare global {
-//   interface Window {
-//     smartsupp: any;
-//     _smartsupp: any;
-//   }
-// }
-// //commit changes
-// export default function SmartSupp() {
-//   useEffect(() => {
-//     window._smartsupp = window._smartsupp || {};
-//     window._smartsupp.key = "572cdc8ab159c994ef97c92c4dc85aa8867c5721";
-//     window.smartsupp =
-//       window.smartsupp ||
-//       function () {
-//         (window.smartsupp._ = window.smartsupp._ || []).push(arguments);
-//       };
-//     window.smartsupp._ = window.smartsupp._ || [];
+declare global {
+  interface Window {
+    smartsupp: any;
+    _smartsupp: any;
+  }
+}
+//commit changes
+export default function SmartSupp() {
+  // useEffect(() => {
+  //   window._smartsupp = window._smartsupp || {};
+  //   window._smartsupp.key = "572cdc8ab159c994ef97c92c4dc85aa8867c5721";
+  //   window.smartsupp =
+  //     window.smartsupp ||
+  //     function () {
+  //       (window.smartsupp._ = window.smartsupp._ || []).push(arguments);
+  //     };
+  //   window.smartsupp._ = window.smartsupp._ || [];
 
-//     const script = document.createElement("script");
-//     script.type = "text/javascript";
-//     script.async = true;
-//     script.src = "https://www.smartsuppchat.com/loader.js?";
-//     document.head.appendChild(script);
+  //   const script = document.createElement("script");
+  //   script.type = "text/javascript";
+  //   script.async = true;
+  //   script.src = "https://www.smartsuppchat.com/loader.js?";
+  //   document.head.appendChild(script);
 
-//     return () => {
-//       document.head.removeChild(script);
-//     };
-//   }, []);
+  //   return () => {
+  //     document.head.removeChild(script);
+  //   };
+  // }, []);
 
-//   return null;
-// }
+  return null;
+}
