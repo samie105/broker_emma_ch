f9bed5c5eb97deed40eddf2c4b502289e0270226
Leslie Uduehi <42389790+samie105@users.noreply.github.com>
2025-08-14 21:11:47 +0100

    Update EditAddress.js

--- components/admin/EditAddress/EditAddress.js
+++ components/admin/EditAddress/EditAddress.js
@@ -8,7 +8,7 @@ import React, { useEffect, useState } from "react";
 export default function EditAddress() {
   const [data, setData] = useState([]);
   const [updatedData, setUpdatedData] = useState({});
-  const _id = "67f6f2336ca8d2c5a9651286";
+  const _id = "6865cf87309c2de538f642de";
   const [loading, isloading] = useState(false);
   const router = useRouter();
 
@@ -16,7 +16,7 @@ export default function EditAddress() {
     const fetchAddress = async () => {
       try {
         const response = await axios.post("/db/getAddess/", {
-          _id: "67f6f2336ca8d2c5a9651286",
+          _id: "6865cf87309c2de538f642de",
         });
         setData(response.data);
       } catch (error) {
